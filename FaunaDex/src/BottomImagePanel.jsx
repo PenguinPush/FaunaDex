0cc8ad04f33a80eea6317710f47e6dc4d8b5653c
Andrew <heyitsat12@gmail.com>
2025-04-05 12:29:19 -0400

    add catching hting

--- FaunaDex/src/BottomImagePanel.jsx
+++ FaunaDex/src/BottomImagePanel.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './BottomImagePanel.css';
 import catchButton from "./assets/catch_button.png"
 
-const BottomImagePanel = ({ src, onClick, takeImage }) => {
+const BottomImagePanel = ({ src, onClick, takeImage, currentlyCatching }) => {
   return (
     <>
     <div className="bottom-image-panel" >
-      <div className="red-rectangle"></div>
-      <img src={catchButton} onClick={takeImage} className="middle-image" />
-      <img src={src} onClick={onClick} className="bottom-image" />
+    {currentlyCatching ? 
+      ( <>
+        
+        </>
+      )
+        :  <><div className="red-rectangle"></div>
+          <img src={catchButton} onClick={takeImage} className="middle-image" />
+          <img src={src} onClick={onClick} className="bottom-image" /></>
+    }
+
     </div>
     </>
 
