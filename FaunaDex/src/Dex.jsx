a02a74a6029117b0c306305a9b11e2e6ab5b2f60
Edward Wang <edward.wang308@gmail.com>
2025-04-05 17:45:01 -0400

    save origial photo

--- FaunaDex/src/Dex.jsx
+++ FaunaDex/src/Dex.jsx
@@ -8,13 +8,8 @@ const Dex = ({ onBack, setPage, setPokemon }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log("fetching");
-
         const response = await fetch('https://fauna-dex-3f13cfbb2cab.herokuapp.com/get_dex');
-        console.log("fetching");
-
         const result = await response.json();
-        console.log(response);
         if (result.status === 'success') {
           setItems(result.data);
         } else {
@@ -23,10 +18,9 @@ const Dex = ({ onBack, setPage, setPokemon }) => {
       } catch (error) {
         console.error('Error fetching data:', error);
       }
-    }
-    fetchData();   
-    }
-  ,[])
+    };
+    fetchData();
+  }, []);
 
   const handleItemClick = (name) => {
     setPage("description");
@@ -34,23 +28,23 @@ const Dex = ({ onBack, setPage, setPokemon }) => {
 
   return (
     <>
-    <button className="back-button" onClick={onBack}>← Back</button>
-    <div className="dex-container">
-      <div className="dex-grid">
-        {items.map((item, index) => (
-          <DexItem
-            key={index}
-            image={"https://fauna-dex-3f13cfbb2cab.herokuapp.com/" + item.image_path}
-            name={item.name}
-            onClick={() => {
-              handleItemClick(item.name);
-              setPokemon(item);
-            }}
-          />
-        ))}
+      <button className="back-button" onClick={onBack}>← Back</button>
+      <div className="dex-container">
+        <div className="dex-grid">
+          {items.map((item, index) => (
+            <DexItem
+              key={index}
+              image={"https://fauna-dex-3f13cfbb2cab.herokuapp.com/" + item.image_path}
+              name={item.name}
+              onClick={() => {
+                handleItemClick(item.name);
+                setPokemon(item);
+              }}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-     </>
+    </>
   );
 };
 
