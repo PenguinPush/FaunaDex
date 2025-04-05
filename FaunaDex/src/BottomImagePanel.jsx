0ee6a3d4b90d26ec538681b65b271dcb9c248a82
Andrew <heyitsat12@gmail.com>
2025-04-05 13:17:59 -0400

    catch failed functionality

--- FaunaDex/src/BottomImagePanel.jsx
+++ FaunaDex/src/BottomImagePanel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './BottomImagePanel.css';
 import catchButton from "./assets/catch_button.png"
 
-const BottomImagePanel = ({ src, catchSuccess, setPage, onClick, takeImage, currentlyCatching }) => {
+const BottomImagePanel = ({ src, setCatchSuccess, setCatchFailed, catchFailed, catchSuccess, setPage, onClick, takeImage, currentlyCatching }) => {
   return (
     <>
     <div className="bottom-image-panel" >
@@ -11,7 +11,8 @@ const BottomImagePanel = ({ src, catchSuccess, setPage, onClick, takeImage, curr
           <p></p>
         </>
       )
-        :  catchSuccess ? <p onClick={() => {setPage("dex")}}>Catch successful! Click to see it in your dex</p>
+        : catchFailed ? <p onClick={() => {setCatchSuccess(false); setCatchFailed(false)}}>Catch failed! There is no animal in the picture</p>
+        :  catchSuccess ? <p onClick={() => {setPage("dex"); setCatchSuccess(false); setCatchFailed(false)}}>Catch successful! Click to see it in your dex</p>
         : <><div className="red-rectangle"></div>
           <img src={catchButton} onClick={takeImage} className="middle-image" />
           <img src={src} onClick={onClick} className="bottom-image" /></>
