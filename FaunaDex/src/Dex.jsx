fda5da046e6cca2c5f515d866bf6014ff0fadefe
Andrew <heyitsat12@gmail.com>
2025-04-05 15:08:40 -0400

    test

--- FaunaDex/src/Dex.jsx
+++ FaunaDex/src/Dex.jsx
@@ -8,11 +8,6 @@ const Dex = ({onBack, setPage, setPokemon}) => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-<<<<<<< HEAD
-    setItems([
-      {name: "squirtle",
-       image: "uploads/20250405173140_snapshot.jpg"
-=======
     const fetchData = async () => {
       try {
         const response = await fetch('https://fauna-dex-3f13cfbb2cab.herokuapp.com/get_dex');
@@ -24,25 +19,8 @@ const Dex = ({onBack, setPage, setPokemon}) => {
         }
       } catch (error) {
         console.error('Error fetching data:', error);
->>>>>>> 5a67f9554ea2449d3630c5b0bb86463cafcdf36d
-      }
-    ])
-    // const fetchData = async () => {
-    //   try {
-    //     const response = await fetch('http://127.0.0.1:5050/get_dex');
-    //     const result = await response.json();
-    //     if (result.status === 'success') {
-    //       setItems(result.data);
-    //     } else {
-    //       console.error('Failed to fetch data:', result.message);
-    //     }
-    //   } catch (error) {
-    //     console.error('Error fetching data:', error);
-    //   }
-    // };
-
-    // fetchData();
-  }, []);
+      }}}
+  )
 
   const handleItemClick = (name) => {
     setPage("description");
