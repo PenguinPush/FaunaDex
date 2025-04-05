c0345da94245f334dd2ee9806d7ddfe95717490d
PenguinPush <icyzminecraft@gmail.com>
2025-04-05 17:59:44 -0400

    sdfsdf

--- server.py
+++ server.py
@@ -6,13 +6,14 @@ from animal import Animal
 from mongodb import database_update, database_fetch
 from cloud_storage import upload_image, fetch_image
 from cropper import crop_to_animal
+from OpenSSL import SSL
 
 
 app = Flask(__name__)
 CORS(app)
 
 # Ensure the uploads folder exists
-UPLOAD_FOLDER = 'saves'
+UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
@@ -70,4 +71,4 @@ def get_dex():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5050))
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=True, ssl_context='adhoc')
