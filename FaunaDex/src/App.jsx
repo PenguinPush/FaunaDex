64a6f5c5d1eb154f410026b84947609f2558fc95
Andrew <heyitsat12@gmail.com>
2025-04-05 09:55:50 -0400

    description page

--- FaunaDex/src/App.jsx
+++ FaunaDex/src/App.jsx
@@ -4,13 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Camera from './Camera'
 import Dex from './Dex'
+import Description from './Description'
 
 function App() {
   const [page, setPage] = useState(0)
 
   return (
     <>
-      <Dex />
+      {/* <Dex /> */}
+      <Description name="Squirtle" description="Hello world!" timesCaught={1} image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"/>
     </>
   )
 }
