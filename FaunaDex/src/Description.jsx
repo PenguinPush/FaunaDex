3ce454c62124431d0d76dde5f8a594c273b5e14c
Edward Wang <edward.wang308@gmail.com>
2025-04-05 18:30:06 -0400

    bruh

--- FaunaDex/src/Description.jsx
+++ FaunaDex/src/Description.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Description.css';
 
-const Description = ({ title, timesCaught, description, image, onBack }) => {
+const Description = ({ name, timesCaught, description, image, onBack }) => {
   return (
     <div className="pokemon-description-page">
       <button className="back-button" onClick={onBack}>← Back</button>
       <div className="pokemon-description">
         <div className="background-rectangle">
-          <img src={image} alt={title} className="background-image" />
+          <img src={image} className="background-image" />
         </div>
         <div className="pokemon-caught-position">
           Caught: {timesCaught} time{timesCaught !== 1 ? 's' : ''}
@@ -15,7 +15,7 @@ const Description = ({ title, timesCaught, description, image, onBack }) => {
         <div className="Lgray-rectangle"></div>
         <div className="gray-rectangle"></div>
         <div className="pokemon-info">
-          <p className="pokemon-title">{title}</p>
+          <p className="pokemon-title">{name}</p>
           <p className="pokemon-title">{description}</p>
         </div>
       </div>
